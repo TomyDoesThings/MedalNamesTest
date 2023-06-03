99136737e3c2254cae0c1ef5c5781887882390f5
TomyDoesThings <119363392+TomyDoesThings@users.noreply.github.com>
2023-06-03 03:18:33 -0400

    Note Fix

--- src/MedalNamesTest.java
+++ src/MedalNamesTest.java
@@ -210,8 +210,10 @@ public class MedalNamesTest
 
             /*
                Literally this whole switch-case is easter eggs. lol
-               Also, there is so much language here that in a way, this is to maybe repulse people from looking at the code?
+               Also, there was so much language here that in a way, this was to maybe repulse people from looking at the code, or maybe for them to laugh when they type the random curse words?
                lol
+
+               To make the code clean for everybody to look at though, I removed them.
              */
         }
 
